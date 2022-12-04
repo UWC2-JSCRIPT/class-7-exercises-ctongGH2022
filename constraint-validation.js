bbe5b7aa70d732a8c0ec7dfbe195c4706c69f3bb
Cecilia <ctong10@uw.edu>
2022-12-03 22:06:56 -0800

    completed Constraint-Vadilation js

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,1 +1,28 @@
+/**
+ * Cecilia Tong
+ * Class 7 Exercise & Homework
+ * Nov 29, 2022
+ * CONSTRAINT VALIDATION
+ */
+
 // TODO
+
+//function to validate firstname, lastname and email
+function validateInput(id) {
+    const inputVal = document.getElementById(id);
+    inputVal.addEventListener("input", () => {
+        inputVal.setCustomValidity("");
+        if (inputVal.checkValidity() === false) {
+            if (id == "email") {
+                inputVal.setCustomValidity(` ${id} must be a correct format for an email (i.e. 'e@e')`);
+            } else {
+                inputVal.setCustomValidity(` ${id} must be from 2 to 25 letters!!!`);
+            }
+        }; //end if
+    }); //end event
+} //end function
+
+//call function validateInput() to validate firstname, lastname and email
+validateInput('first-name');
+validateInput('last-name');
+validateInput("email");
